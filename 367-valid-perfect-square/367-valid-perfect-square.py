f3bd0ca6f521bbe4f5aae3fbaca1f8a76cb19230
B Varun Gupta <58597383+bvarungupta45@users.noreply.github.com>
2022-08-05 10:54:21 +0530

    Time: 59 ms (25.76%), Space: 13.8 MB (96.59%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if int(num**0.5)**2==num:
+            return True
+        else:
+            return False
+       
+            
+            
+        
